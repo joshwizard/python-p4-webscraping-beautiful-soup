fd5dbd78a96a59dda6d9ab10aeaebfd7a9ba1eea
joshwizard <murikij@gmail.com>
2026-01-21 23:01:34 +0300

    HTML scraping

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,12 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-financier'))
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses: 
+  print(course.contents[0].strip())
